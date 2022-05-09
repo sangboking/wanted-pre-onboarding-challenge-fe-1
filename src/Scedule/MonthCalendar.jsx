0674e6364f6ff2e89b8b876d20dd93de2d39df16
sangboking <cowg001@naver.com>
2022-05-09 11:19:14 +0900

    test2

--- src/Scedule/MonthCalendar.jsx
+++ src/Scedule/MonthCalendar.jsx
@@ -33,9 +33,7 @@ const CalendarTable = styled.table`
   box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.16);
 `;
 
-const CalendarThead = styled.thead`
-  
-`;
+const CalendarThead = styled.thead` `;
 
 const CalendarTr = styled.tr`
   
